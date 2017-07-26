e644b63f1709dd0ee184796d7de46a799e8b8c2f
DL-BrownBag <rcouch@npauctions.com>
2017-07-26 06:28:47 -0700

    initial push

---
+++ firebase.conf.ts
@@ -0,0 +1,12 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDxza9sVihgWT9FRcYhO1yoj7C8hY9Bru8",
+  authDomain: "burrito-wednesdays.firebaseapp.com",
+  databaseURL: "https://burrito-wednesdays.firebaseio.com",
+  projectId: "burrito-wednesdays",
+  storageBucket: "",
+  messagingSenderId: "782736677054" 
+};
+
+
+
